3165ad46358a2326fb02e48d8550a4010269b168
Administrator <c.uvsuv@163.com>
2019-06-14 18:02:14 +0800

    update

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,21 @@
+object Versions{
+    val compileSdkVersion = 28
+
+    val buildToolsVersion = "28.0.3"
+
+    val minSdkVersion = 24
+
+    val targetSdkVersion = 28
+}
+
+
+
+object LibV{
+
+
+    val rxlifecycle3 ="3.0.0"
+    val rxjava = "2.2.6"
+    val rxandroid = "2.1.1"
+    val rxkotlin = "2.3.0"
+    val glide = "4.9.0"
+}
