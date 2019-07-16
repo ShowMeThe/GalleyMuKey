06c6c67492835167e1f6a0496b49dd4c5aaa49d4
ken <c.uvsuv@163.com>
2019-07-16 22:15:18 +0800

    fix picker

--- kframework/src/main/java/showmethe/github/kframework/util/widget/ViewUtils.kt
+++ kframework/src/main/java/showmethe/github/kframework/util/widget/ViewUtils.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.os.Build
 import com.google.android.material.tabs.TabLayout
 import android.util.DisplayMetrics
+import android.view.View
 import android.widget.LinearLayout
 import android.widget.PopupWindow
 import android.widget.TextView
@@ -17,6 +18,27 @@ import java.lang.reflect.Field
  * Date : 2018/9/18
  * Time : 15:35
  */
+
+
+fun View.setOnSingleClickListner(onSingleClick : (it:View)->Unit){
+    var lastClickTime = 0L
+    val lastLongTime = 2000L
+
+    setOnClickListener {
+        val time = System.currentTimeMillis()
+        if(time - lastClickTime > lastLongTime){
+            onSingleClick.invoke(it)
+            lastClickTime = time
+        }
+    }
+}
+
+
+
+
+
+
+
 object ViewUtils {
     /**
      * 反射获取mLayoutScreen 设置对象为true,全屏效果，覆盖状态栏
