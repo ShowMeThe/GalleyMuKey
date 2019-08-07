fd0376515f45fa14a7ad93474b5af9499746d067
ken <c.uvsuv@163.com>
2019-08-07 22:38:03 +0800

    add ext and interval class

--- kframework/src/main/java/showmethe/github/kframework/util/extras/Interval.kt
+++ kframework/src/main/java/showmethe/github/kframework/util/extras/Interval.kt
@@ -1,23 +1,25 @@
-package showmethe.github.kframework.util.extras
+package com.example.ken.kmvvm
 
 import android.os.Handler
 import android.os.Message
 import java.lang.ref.WeakReference
 
-/**
- *  动作静止后一段时间执行对应内容  过滤频繁触发 如 vp切换页面时候触发网络请求
- */
-class Interval(var delay:Long,var filter:Long = 250L) {
 
-    private var handler = Handler(
-        WeakReference(Handler.Callback {
-            when(it.what){
-                  step -> {
-                      onMethod?.invoke()
-                  }
-            }
-        true
-    }).get())
+
+class Interval(private var delay:Long,private var filter:Long = 250L) {
+
+    private var handler : WeakReference<Handler>? = null
+    init {
+        handler = WeakReference(Handler(
+            WeakReference(Handler.Callback {
+                when(it.what){
+                    step -> {
+                        onMethod?.invoke()
+                    }
+                }
+                true
+            }).get()))
+    }
 
     private var step = 0
     private var lastStep = 0
@@ -26,20 +28,24 @@ class Interval(var delay:Long,var filter:Long = 250L) {
 
     fun start(onMethod : (()->Unit)){
         step ++
-       val last  = System.currentTimeMillis() - lastTime
+        val last  = System.currentTimeMillis() - lastTime
         if(last>filter){
             this.onMethod = onMethod
             lastTime = last
-            handler.sendMessageDelayed(Message.obtain(handler,step),delay)
+            handler?.get()?.sendMessageDelayed(Message.obtain(handler?.get(),step),delay)
         }
         if(lastStep > step){
-            handler.removeMessages(lastStep)
+            handler?.get()?.removeMessages(lastStep)
         }
         lastStep = step
     }
 
+
     fun clean(){
-        handler.removeCallbacksAndMessages( null)
+        handler?.get()?.removeCallbacksAndMessages( null)
+        if(handler != null){
+            handler = null
+        }
     }
 
 }
